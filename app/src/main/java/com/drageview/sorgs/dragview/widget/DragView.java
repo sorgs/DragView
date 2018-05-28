f565ed0fae43200ca5f160fc2ac2bb06a7bbb196
sorgs <1042746391@qq.com>
2018-05-29 00:07:44 +0800

    fix

--- app/src/main/java/com/drageview/sorgs/dragview/widget/DragView.java
+++ app/src/main/java/com/drageview/sorgs/dragview/widget/DragView.java
@@ -48,9 +48,9 @@ public class DragView extends View {
      */
     private float mParentWidth;
     /**
-     * 子空间的高度
+     * 子控件的高度
      */
-    private float mChildHeight;
+    private float mSelectHeight;
     /**
      * 屏幕宽度
      */
@@ -67,17 +67,6 @@ public class DragView extends View {
 
     }
 
-    public DragView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        init(context);
-    }
-
-    public DragView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init(context);
-    }
-
-
     /**
      * 初始化
      */
@@ -91,42 +80,15 @@ public class DragView extends View {
 
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        mParentHeight = MeasureSpec.getSize(heightMeasureSpec);
-        mScreenWidth = MeasureSpec.getSize(widthMeasureSpec);
-        //父控件宽度 16:9的宽
-        mParentWidth = mParentHeight * 9 / 16f;
-        //选中区域高度
-        mChildHeight = mParentWidth * 9 / 16f;
-        initCalc();
+    public DragView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
     }
 
-    /**
-     * 坐标点的计算
-     * X轴基本不变，变化的是Y轴
-     */
-    private void initCalc() {
-        //计算父控件的位置点
-        mPX1 = (int) (mScreenWidth / 2f - mParentWidth / 2f);
-        mPY1 = 0;
-        mPX2 = (int) (mScreenWidth / 2f + mParentWidth / 2f);
-        mPY2 = (int) (mParentHeight);
 
-        //刚开始子控件1的位置点
-        mC1X1 = mPX1;
-        mC1Y1 = mPY1;
-        mC1X2 = mPX2;
-        mC1Y2 = mPY1;
-
-        //刚开始子控件2的位置点
-        mC2X1 = mPX1;
-        mC2Y1 = (int) (mChildHeight);
-        mC2X2 = mPX2;
-        mC2Y2 = mPY2;
-
-
+    public DragView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -143,7 +105,7 @@ public class DragView extends View {
                     if (beginY < mC1Y2) {
                         //起始点在选择框上部，不做反应
                         return false;
-                    } else if (beginY > mC1Y2 + mChildHeight) {
+                    } else if (beginY > mC1Y2 + mSelectHeight) {
                         //起始点在选择框下部，不做反应
                         return false;
                     }
@@ -157,7 +119,7 @@ public class DragView extends View {
                     //更改子控件坐标
                     mC1Y2 = (int) (event.getY() - mDistanceY);
 
-                    mC2Y1 = (int) (event.getY() - mDistanceY + mChildHeight);
+                    mC2Y1 = (int) (event.getY() - mDistanceY + mSelectHeight);
 
                     //往上滑动
                     if (mC1Y2 < 0) {
@@ -167,13 +129,13 @@ public class DragView extends View {
                         mC1Y2 = 0;
 
                         //子控件2为最大
-                        mC2Y1 = (int) (mChildHeight);
+                        mC2Y1 = (int) (mSelectHeight);
 
-                    } else if (mC1Y2 > mParentHeight - mChildHeight) {
+                    } else if (mC1Y2 > mParentHeight - mSelectHeight) {
                         //防止底部超过
 
                         //子控件1为最大
-                        mC1Y2 = (int) (mParentHeight - mChildHeight);
+                        mC1Y2 = (int) (mParentHeight - mSelectHeight);
                         //子控件2为0
                         mC2Y1 = (int) (mParentHeight);
                     }
@@ -207,7 +169,7 @@ public class DragView extends View {
 
         //绘制父控件
 
-        // 指定图片绘制区域(左上角的四分之一)
+        // 指定图片绘制区域
         Rect src = new Rect(0, 0, mParentBg.getWidth(), mParentBg.getHeight());
         // 指定图片在屏幕上显示的区域
         Rect dst = new Rect(mPX1, mPY1, mPX2, mPY2);
@@ -219,6 +181,42 @@ public class DragView extends View {
 
         //绘制子控件2
         canvas.drawRect(mC2X1, mC2Y1, mC2X2, mC2Y2, mChildPaint);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mParentHeight = MeasureSpec.getSize(heightMeasureSpec);
+        mScreenWidth = MeasureSpec.getSize(widthMeasureSpec);
+        //父控件宽度 16:9的宽
+        mParentWidth = mParentHeight * 9 / 16f;
+        //选中区域高度
+        mSelectHeight = mParentWidth * 9 / 16f;
+        initCalc();
+    }
+
+    /**
+     * 坐标点的计算
+     * X轴基本不变，变化的是Y轴
+     */
+    private void initCalc() {
+        //计算父控件的位置点
+        mPX1 = (int) (mScreenWidth / 2f - mParentWidth / 2f);
+        mPY1 = 0;
+        mPX2 = (int) (mScreenWidth / 2f + mParentWidth / 2f);
+        mPY2 = (int) (mParentHeight);
+
+        //刚开始子控件1的位置点
+        mC1X1 = mPX1;
+        mC1Y1 = mPY1;
+        mC1X2 = mPX2;
+        mC1Y2 = mPY1;
+
+        //刚开始子控件2的位置点
+        mC2X1 = mPX1;
+        mC2Y1 = (int) (mSelectHeight);
+        mC2X2 = mPX2;
+        mC2Y2 = mPY2;
     }
 
     /**
@@ -242,7 +240,7 @@ public class DragView extends View {
         //截图指定部分
         if (bitmap != null) {
             bitmap = Bitmap.createBitmap(bitmap, mC1X1, mC1Y2 + getStatusBarHeight(),
-                    (int) mParentWidth, (int) mChildHeight);
+                    (int) mParentWidth, (int) mSelectHeight);
         }
         invalidate();
         return bitmap;
